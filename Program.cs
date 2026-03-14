9efeb6b61f1abe1e3880bdd67d654d0daa9c5848
Nazar Kokovkin <lilnz4@hotmail.com>
2026-03-14 22:00:04 +0100

    Add files via upload
    first commit: added project files and classes

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GameControlSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
